a1d225adb83635cdec0a43aad92f39967d8e50f5
Adiletbaike <adiletdzhuraev@gmail.com>
2025-11-28 00:16:22 +0100

    DEV-25 FLyway is working

--- backend/src/main/java/com/m01project/taskmanager/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/m01project/taskmanager/demo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.m01project.taskmanager.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/auth/**", "/api/test/db"))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**","/api/test/db").permitAll()
                         .requestMatchers("/api/users/**").authenticated())
